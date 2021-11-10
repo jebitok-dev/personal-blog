aa2ff9317e3a43007cc18bee96ad7b9964a83961
jebitok-dev <sharonkosgei4@gmail.com>
2021-11-10 18:45:45 +0300

    Add Changes to Components

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import {PostCard, Categories, PostWidget} from '../components/index';
 import {getPosts} from "../services";
 
-/* eslint-disable*/ 
+// /* eslint-disable*/ 
 export default function Home({posts}) {
   return (
     <div className="container mx-auto px-10 mb-8">
