1cabfebd1f34ec0317d16038ecc234b6c440ce48
jebitok-dev <sharonkosgei4@gmail.com>
2021-11-10 15:09:31 +0300

    Add Changes to Services and Components

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
 import Head from 'next/head';
 import {PostCard, Categories, PostWidget} from '../components/index';
+import {getPosts} from "../services";
 
-const posts = [
-  {
-    title: "React Intro",
-    excerpt: "React is an open-source Javascript library made by developer at Facebook",
-  },
-  {
-    title: "Setting up React with Tailwind CSS",
-    excerpt: "React can be styled using Tailwind CSS which is popular CSS Framework",
-  }
-];
 /* eslint-disable*/ 
-export default function Home() {
+export default function Home({posts}) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -21,7 +12,7 @@ export default function Home() {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => <PostCard post={post} key={post.title} /> )}
+          {posts.map((post) => <PostCard post={post.node} key={post.title} /> )}
         </div>
         <div className="lg:col-span-4 col-span1">
             <div className="lg:sticky relative top-8">
@@ -32,4 +23,12 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+
+  return {
+    props: {posts},
+  }
 }
