576c3d456762643b42861e1efbdd8637c3b1671b
jebitok-dev <sharonkosgei4@gmail.com>
2021-11-12 19:46:05 +0300

    Add Changes to Comments

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -4,24 +4,28 @@
 * will be treated as an API endpoint instead of a page.         *
 *************************************************************** */
 
-import { GraphQLClient, gql } from "graphql";
+import { GraphQLClient, gql } from 'graphql';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
-export default function comments(req, res) {
+export default async function comments(req, res) {
   const graphQLClient = new GraphQLClient(graphqlAPI, {
     headers: {
-      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`
-    }
-  })
+      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+    },
+  });
 
   const query = gql`
     mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
       createComment(data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}) {id}
     }
-  `
+  `;
 
-  const result = await graphQLClient.request(query, req.body)
+  try {
+    const result = await graphQLClient.request(query, req.body);
+  } catch(error) {
+    
+  }
 
   return res.status(200).send(result);
 }
