abf3b2ae21bc69486ac0dd1e98e6c0e129d035a5
jebitok-dev <sharonkosgei4@gmail.com>
2021-11-19 23:28:24 +0300

    Add Changes to Components

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import Link from 'next/link';
 import { getCategories } from '../services';
+/* eslint-disable*/ 
 
 const Header = () => {
   const [categories, setCategories] = useState([]);
